3fc21b1543a1c338efa30cb75aa659cd479ca3f0
Antonioafj <antonioafj.edu@gmail.com>
2024-05-01 13:10:21 -0400

    utilizando scanner

--- java-terminal-executavel/src/SobreMim.java
+++ java-terminal-executavel/src/SobreMim.java
@@ -1,12 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class SobreMim {
     public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        System.out.println("Digite Seu Nome");
+        String nome = scanner.next();
 
-        System.out.println("Ola, me chamo " + nome  + "   " + sobrenome);
+        System.out.println("Digite Seu Sobrenome");
+        String sobrenome = scanner.next();
+        
+        System.out.println("Digite Sua Idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite Sua Altura");
+        Double altura = scanner.nextDouble();
+
+
+        System.out.println("Ola, me chamo " + nome + "  " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha Altura é " + altura + " cm ");
     }
